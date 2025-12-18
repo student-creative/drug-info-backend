85f9c7b9e7be1e5279c287780f96f30d5eeca7b1
student-creative <aryasutariya3@gmail.com>
2025-12-18 14:56:28 +0530

    fronted url paste cros

--- app.js
+++ app.js
@@ -8,15 +8,16 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-const cors = require("cors");
-
-
+const cors = require('cors');
+app.use(cors({
+  origin: 'https://druginformation.netlify.app'
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
